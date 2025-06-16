4a11b235eba944fbe3d62889bb6726088dba9419
Richard <richet9@gmail.com>
2025-06-16 18:32:25 -0400

    first commit

---
+++ TestDrive.lua
@@ -0,0 +1,60 @@
+source(g_currentModDirectory .. "TestDrive.lua")
+
+TestDrive = {}
+TestDrive.dir = g_currentModDirectory
+TestDrive.modName = g_currentModName
+
+TestDrive.initialized = false
+TestDrive.vehicle = nil
+TestDrive.manager = nil
+
+ShopConfigScreen.onVehiclesLoaded = Utils.appendedFunction(ShopConfigScreen.onVehiclesLoaded, function(self)
+    TestDrive.createTestDriveButton(self)
+end)
+
+ShopConfigScreen.onClose = Utils.appendedFunction(ShopConfigScreen.onClose, function(self)
+    TestDrive.removeTestDriveButton(self)
+end)
+
+function TestDrive.createTestDriveButton(shopConfigScreen)
+    if shopConfigScreen.testDriveButton ~= nil then
+        return
+    end
+
+    local vehicle = shopConfigScreen.previewVehicles[1]
+    if SpecializationUtil.hasSpecialization(Drivable, vehicle.specializations) ~= true then
+        print("[DEBUG] TestDrive: Not adding button, not a drivable vehicle.")
+        return
+    end
+
+    local testDriveButton = shopConfigScreen.buyButton:clone(shopConfigScreen.buttonsPanel)
+    testDriveButton:setText(g_i18n:getText("rp_TEST_DRIVE"))
+    testDriveButton:setInputAction("MENU_EXTRA_2")
+    testDriveButton.onClickCallback = function()
+        TestDrive.manager:startTestDrive(shopConfigScreen.storeItem)
+    end
+
+    shopConfigScreen.buyButton.parent:addElement(testDriveButton)
+    shopConfigScreen.testDriveButton = testDriveButton
+end
+
+function TestDrive.removeTestDriveButton(shopConfigScreen)
+    if shopConfigScreen.testDriveButton == nil then
+        return -- Already gone.
+    end
+    -- TODO: remove from memory?
+    shopConfigScreen.testDriveButton.parent:removeElement(shopConfigScreen.testDriveButton)
+    shopConfigScreen.testDriveButton = nil
+    print("[DEBUG] TestDrive: Removed test drive button.")
+end
+
+local function init()
+    if TestDrive.initialized then
+        return
+    end
+
+    TestDrive.manager = TestDriveManager.new()
+    TestDrive.initialized = true
+end
+
+init()
---
+++ TestDriveManager.lua
@@ -0,0 +1,43 @@
+TestDriveManager = {}
+TestDriveManager.duration = 5 -- minutes.
+
+local TestDriverManger_mt = Class(TestDriveManager)
+function TestDriveManager.new()
+    self = setmetatable({}, TestDriverManger_mt)
+
+    self.vehicle = nil
+    self.timer = Timer.new(TestDriveManager.duration * 60 * 1000)
+    self.timer:setFinishCallback(function()
+        self:onTimerFinish()
+    end)
+
+    return self
+end
+
+function TestDriveManager:onTimerFinish()
+    InfoDialog.show("Your test drive has finished! The dealer has taken back the vehicle.", function()
+        self.vehicle:delete()
+        self.vehicle = nil
+    end)
+end
+
+function TestDriveManager:startTestDrive(storeItem)
+    if self.vehicle ~= nil then
+        InfoDialog.show("You can only do one test drive at a time!")
+        return
+    end
+
+    local data = VehicleLoadingData.new()
+    data:setStoreItem(storeItem)
+    data:setLoadingPlace(g_currentMission.storeSpawnPlaces, g_currentMission.usedStorePlaces)
+    data:setPropertyState(VehiclePropertyState.OWNED)
+    data:setOwnerFarmId(g_localPlayer.farmId)
+
+    data:load(function(_, loadedvehicles)
+        self.vehicle = loadedvehicles[1]
+    end)
+
+    InfoDialog.show("Your test drive has begun! The dealer will take back the vehicle in 5 minutes.", function()
+        self.timer:start()
+    end)
+end
