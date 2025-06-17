5a21ba0f249e880f8801b2b004cb20e25fb39d76
Richard <richet9@gmail.com>
2025-06-16 23:56:30 -0400

    Add support for pruchasing insurance. Display the message but don't deduct any money yet.

--- TestDriveManager.lua
+++ TestDriveManager.lua
@@ -1,5 +1,7 @@
 TestDriveManager = {}
 TestDriveManager.duration = 5 -- minutes.
+TestDriveManager.insurancePriceThreshold = 100000
+TestDriveManager.insurancePricePercent = 0.002 -- 2% total price
 
 local TestDriverManger_mt = Class(TestDriveManager)
 function TestDriveManager.new()
@@ -27,6 +29,50 @@ function TestDriveManager:startTestDrive(storeItem, configurations)
         return
     end
 
+    local insurancePrice = self:getInsurancePrice(storeItem, configurations)
+    local shouldContinue = insurancePrice == 0
+
+    local function purchaseInsurance(self, yes)
+        if yes then
+            -- TODO: deduct funds before finalizing test drive.
+            self:finalizeTestDrive(storeItem, configurations)
+        end
+    end
+
+    if insurancePrice > 0 then
+        YesNoDialog.show(purchaseInsurance, self,
+                         "The dealer requires that this vehicle have insurance purchased prior to your test drive.\n" ..
+                             ("They are requesting $%s for insurance.\n"):format(insurancePrice) ..
+                             "Do you wish to continue?")
+    else
+        self:finalizeTestDrive(storeItem, configurations)
+    end
+end
+
+function TestDriveManager:getInsurancePrice(storeItem, configurations)
+    local buyPrice = g_currentMission.economyManager:getBuyPrice(storeItem, configurations)
+    if buyPrice < TestDriveManager.insurancePriceThreshold then
+        return 0
+    end
+    return math.ceil(buyPrice * TestDriveManager.insurancePricePercent)
+end
+
+function TestDriveManager:finalizeTestDrive(storeItem, configurations)
+    self:loadVehicle(storeItem, configurations)
+
+    local message = ("Your test drive has begun! The dealer will take back the vehicle in %s minutes."):format(
+                        TestDriveManager.duration)
+
+    InfoDialog.show(message, function()
+        self.timer:setDuration(TestDriveManager.duration * 60 * 1000)
+        self.timer:start()
+        self.vehicle.isTestDriveVehicle = true
+    end)
+
+    TestDrive.removeTestDriveButton(g_gui.screenControllers[ShopConfigScreen])
+end
+
+function TestDriveManager:loadVehicle(storeItem, configurations)
     local data = VehicleLoadingData.new()
     data:setStoreItem(storeItem)
     data:setConfigurations(configurations)
@@ -36,16 +82,6 @@ function TestDriveManager:startTestDrive(storeItem, configurations)
 
     data:load(function(_, loadedvehicles)
         self.vehicle = loadedvehicles[1]
-    end)
-
-    TestDrive.removeTestDriveButton(g_gui.screenControllers[ShopConfigScreen])
-
-    local message = ("Your test drive has begun! The dealer will take back the vehicle in %s minutes."):format(
-                        TestDriveManager.duration)
-    InfoDialog.show(message, function()
-        self.timer:setDuration(TestDriveManager.duration * 60 * 1000)
-        self.timer:start()
-        self.vehicle.isTestDriveVehicle = true
     end)
 end
 
